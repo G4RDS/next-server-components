0b79c30e98c2313c2edd5a8c554e870eec0f3c20
G4RDS <account@g4rds.jp>
2021-01-30 15:26:23 +0900

    Move header declaration to page component to SSR

--- components/App.server.js
+++ components/App.server.js
@@ -11,47 +11,37 @@ import NoteListSkeleton from './NoteListSkeleton'
 
 export default function App({ selectedId, isEditing, searchText, login }) {
   return (
-    <div className="container">
-      <div className="banner">
-        <a
-          href="https://vercel.com/blog/everything-about-react-server-components"
-          target="_blank"
-        >
-          Learn more →
-        </a>
-      </div>
-      <div className="main">
-        <input type="checkbox" className="sidebar-toggle" id="sidebar-toggle" />
-        <section className="col sidebar">
-          <section className="sidebar-header">
-            <img
-              className="logo"
-              src="logo.svg"
-              width="22px"
-              height="20px"
-              alt=""
-              role="presentation"
-            />
-            <strong>React Notes</strong>
-          </section>
-          <section className="sidebar-menu" role="menubar">
-            <SearchField />
-            <AuthButton login={login} noteId={null}>
-              Add
-            </AuthButton>
-          </section>
-          <nav>
-            <Suspense fallback={<NoteListSkeleton />}>
-              <NoteList searchText={searchText} />
-            </Suspense>
-          </nav>
+    <div className="main">
+      <input type="checkbox" className="sidebar-toggle" id="sidebar-toggle" />
+      <section className="col sidebar">
+        <section className="sidebar-header">
+          <img
+            className="logo"
+            src="logo.svg"
+            width="22px"
+            height="20px"
+            alt=""
+            role="presentation"
+          />
+          <strong>React Notes</strong>
         </section>
-        <section className="col note-viewer">
-          <Suspense fallback={<NoteSkeleton isEditing={isEditing} />}>
-            <Note login={login} selectedId={selectedId} isEditing={isEditing} />
+        <section className="sidebar-menu" role="menubar">
+          <SearchField />
+          <AuthButton login={login} noteId={null}>
+            Add
+          </AuthButton>
+        </section>
+        <nav>
+          <Suspense fallback={<NoteListSkeleton />}>
+            <NoteList searchText={searchText} />
           </Suspense>
-        </section>
-      </div>
+        </nav>
+      </section>
+      <section className="col note-viewer">
+        <Suspense fallback={<NoteSkeleton isEditing={isEditing} />}>
+          <Note login={login} selectedId={selectedId} isEditing={isEditing} />
+        </Suspense>
+      </section>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,18 @@
 import Root from '../components/Root.client'
 
-export default Root
+const Index = () => {
+  return (
+    <div className="container">
+      <div className="banner">
+        <a
+          href="https://vercel.com/blog/everything-about-react-server-components"
+          target="_blank"
+        >
+          Learn more →
+        </a>
+      </div>
+      <Root />
+    </div>
+  )
+}
+export default Index
